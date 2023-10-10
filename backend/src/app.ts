c5c4227103864631b95d719284a821acceba36e9
Sakaria Nghivafe <snghivafe@gmail.com>
2023-10-10 22:47:02 +0200

    Updated CORS Configurations

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,9 @@ app.get('/', (req, res) => {
     res.send('Hello TypeScript!');
 });
 
-app.use(cors());
+app.use(cors(
+    corsConfig
+));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', TodoRouter);
