cfd6f098d413e0bc1667d58e95bb4f7998b9b27e
Sakaria Nghivafe <snghivafe@gmail.com>
2023-10-27 00:52:52 +0200

    Created Update User

--- backend/src/api/auth/controller.ts
+++ backend/src/api/auth/controller.ts
@@ -47,12 +47,20 @@ export const getUser = async (req: Request, res: Response, next: NextFunction) =
     }
 }
 
-export const createTodo = async (req: Request, res: Response, next: NextFunction) => {
+export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {title} = req.body;
-        const todo = await prisma.todo.create({
-            data: {
-                title: title,
+        const user = await prisma.user.update({
+            where: {
+                id: req.params.id
+            },
+            data: req.body
+        });
+
+        res.status(200).json({message: "User updated successfully"});
+    } catch (error: any) {
+        res.status(500).json({message: "Login failed"})
+    }
+}
             }
         });
         res.json(todo);
