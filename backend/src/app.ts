cc27623ba902776bb0f23eb866481e814de28148
Sakaria Nghivafe <snghivafe@gmail.com>
2023-10-28 00:52:53 +0200

    Created Event Logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { AuthRouter } from './api/auth';
 import { EventRouter } from './api/event';
 import { MailRouter } from './api/mail';
+import { LogRouter } from './api/log';
 import cors from 'cors';
 
 const app = express();
@@ -24,6 +25,7 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api', AuthRouter);
 app.use('/api', EventRouter);
 app.use('/api', MailRouter);
+app.use('/api', LogRouter);
 
 
 export default app;
