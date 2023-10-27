446b633abe884ed22a8070caee53b2576f8590ce
br1ck7 <veikko.kapiya@gmail.com>
2023-10-27 11:55:29 +0200

    implemented group

--- backend/src/api/group/controller.ts
+++ backend/src/api/group/controller.ts
@@ -1,42 +1,88 @@
 import { Express, Request, Response, NextFunction } from "express";
 import prisma from "../../database";
 
+export const getGroups = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const groups = await prisma.group.findMany();
+		//console.log(todos);
+		res.json(groups);
+	} catch (error: any) {
+		res.status(500).json({ message: error.message });
+	}
+};
 
-export const getTodos = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const todos = await prisma.todo.findMany();
-        //console.log(todos);
-        res.json(todos);
-    } catch (error: any) {
-        res.status(500).json({message: error.message})
-    }
-}
+export const getGroup = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { id } = req.params;
+		const group = await prisma.group.findUnique({
+			where: {
+				id: String(id),
+			},
+		});
+		res.json(group);
+	} catch (error: any) {
+		res.status(500).json({ message: "Get todo failed" });
+	}
+};
 
-export const getTodo = async (req: Request, res: Response, next: NextFunction) => {
+export const createGroup = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { title } = req.body;
+		const group = await prisma.todo.create({
+			data: {
+				title: title,
+			},
+		});
+		res.json(group);
+	} catch (error: any) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+export const updateGroup = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
     try {
-        const {id} = req.params;
-        const todo = await prisma.todo.findUnique({
+        const group = await prisma.group.update({
             where: {
-                id: String(id)
-            }
+                id: String(req.params.id)
+            },
+            data: req.body,
         });
-        res.json(todo);
+        res.json(group);
     } catch (error: any) {
-        res.status(500).json({message: "Get todo failed"})
+        res.status(500).json({message: "Failed to update group"})
     }
-}
+};
 
-export const createTodo = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {title} = req.body;
-        const todo = await prisma.todo.create({
-            data: {
-                title: title,
-            }
-        });
-        res.json(todo);
-    } catch (error: any) {
-        res.status(500).json({message: error.message})
-    }
-}
-
+export const deleteGroup = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { id } = req.params;
+		const group = await prisma.group.delete({
+			where: {
+				id: String(id),
+			},
+		});
+		res.json(group);
+	} catch (error: any) {
+		res.status(500).json({ message: "Failed to delete group" });
+	}
+};
--- backend/src/api/group/route.ts
+++ backend/src/api/group/route.ts
@@ -1,12 +1,19 @@
 import { Router, Response, Request } from "express";
-import { getTodos, getTodo, createTodo } from "./controller";
-
+import {
+	getGroups,
+	getGroup,
+	createGroup,
+	deleteGroup,
+	updateGroup,
+} from "./controller";
 
 const router: Router = Router();
 
+router.route("/group").get(getGroups);
+router.route("/group/:id").get(getGroup);
+router.route("/group").post(createGroup);
+router.route("/group/:id").put(updateGroup);
+router.route("/group/:id").delete(deleteGroup);
 
-router.route('/todos').get(getTodos);
-router.route('/todos/:id').get(getTodo);
-router.route('/todos').post(createTodo);
 
 export default router;
