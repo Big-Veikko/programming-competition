0cb9952f33cf78ce5aa5d14f551a920a11361d07
Jeremy <jeremyhaihambo@gmail.com>
2023-10-27 23:33:10 +0200

    Imported the routes FROM express
    Imported all operations routes

--- backend/src/api/opportunity/route.ts
+++ backend/src/api/opportunity/route.ts
@@ -1,6 +1,14 @@
 import { Router, Response, Request } from "express";
 import { getTodos, getTodo, createTodo } from "./controller";
 
+import { Router, Request, Response } from "express";
+import {
+  getOpps,
+  getOpp,
+  createOpp,
+  updateOpps,
+  deleteOpps,
+} from "./controller";
 
 const router: Router = Router();
 
@@ -9,4 +17,10 @@ router.route('/todos').get(getTodos);
 router.route('/todos/:id').get(getTodo);
 router.route('/todos').post(createTodo);
 
+export default router;router.route("/Opps").get(getOpps);
+router.route("/Opps").get(getOpps);
+router.route("/Opp/:id").get(getOpp);
+router.route("/Opp").post(createOpp);
+router.route("Opp/:id").put(updateOpps);
+router.route("Opp/ :id").delete(deleteOpps);
 export default router;
