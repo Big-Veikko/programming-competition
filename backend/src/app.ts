959ad77a53312936d2280e9683e0c53e80ca0685
br1ck7 <veikko.kapiya@gmail.com>
2023-10-27 11:57:58 +0200

    added routes to university, group and fundraiser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,26 @@
-import express from 'express';
-import {TodoRouter} from './api/todos';
-import cors from 'cors';
+import express from "express";
+import { TodoRouter } from "./api/todos";
+import cors from "cors";
+import { UniversityRouter } from "./api/university";
+import { GroupRouter } from "./api/group";
+import { FundrasierRouter } from "./api/fundraiser";
 
 const app = express();
 const corsConfig = {
-    origin: 'https://programming-competition.vercel.app',
-    optionsSuccessStatus: 200
-}
+	origin: "https://programming-competition.vercel.app",
+	optionsSuccessStatus: 200,
+};
 
-
-app.get('/', (req, res) => {
-    res.send('Hello TypeScript!');
+app.get("/", (req, res) => {
+	res.send("Hello TypeScript!");
 });
 
-app.use(cors(
-    corsConfig
-));
+app.use(cors(corsConfig));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api', TodoRouter);
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", TodoRouter);
+app.use("/api", UniversityRouter);
+app.use("/api", GroupRouter);
+app.use("/api", FundrasierRouter);
 
 export default app;
