b04d77a4844c8871e35ec5ccb00ec5521c21e584
br1ck7 <veikko.kapiya@gmail.com>
2023-10-27 11:55:12 +0200

    implemented fundraiser

--- backend/src/api/fundraiser/route.ts
+++ backend/src/api/fundraiser/route.ts
@@ -1,12 +1,22 @@
 import { Router, Response, Request } from "express";
-import { getTodos, getTodo, createTodo } from "./controller";
-
+import {
+	getFundraisers,
+	getFundraiser,
+	getOngoingFundraisers,
+	createFundraiser,
+	updateFundraiser,
+	deleteFundraiser,
+	approveFundraiser,
+} from "./controller";
 
 const router: Router = Router();
 
-
-router.route('/todos').get(getTodos);
-router.route('/todos/:id').get(getTodo);
-router.route('/todos').post(createTodo);
+router.route("/fundraiser").get(getFundraisers);
+router.route("/fundraiser/:id").get(getFundraiser);
+router.route("/fundraiser").post(createFundraiser);
+router.route("/fundraiser/:id").put(updateFundraiser);
+router.route("/fundraiser/ongoing").get(getOngoingFundraisers);
+router.route("/fundraiser/:id").delete(deleteFundraiser);
+router.route("/fundraiser/approve/:id").put(approveFundraiser);
 
 export default router;
