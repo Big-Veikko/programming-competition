ea25565d819bac223b8aeccf5f1c2bc6bfb35964
Sakaria Nghivafe <snghivafe@gmail.com>
2023-10-28 04:44:18 +0200

    Created University Model

---
+++ frontend/src/services/university/university.model.ts
@@ -0,0 +1,10 @@
+export interface UniversityModel {
+    id: string;
+    name: string;
+    website: string;
+    contact_email: string;
+    contact_phone: string;
+    address: string;
+    logo: string;
+    social: object;
+}
